674f4de9ac30d41ac031a32996a21a7d4cad8bca
Oleh Vlasiuk <olehvlasyik@gmail.com>
2019-06-26 10:47:35 +0300

    base react app

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,15 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 const BUILD_DIR = path.join(__dirname, 'dist');
 const APP_DIR = path.join(__dirname, 'src');
 
-/*const VENDOR_LIBS = [
+const VENDOR_LIBS = [
   'react', 'react-dom', 'react-router-dom'
-];*/
+];
 
 const config = {
-  entry: APP_DIR + '/index.js',
-  /*entry: {
+  entry: {
     bundle: APP_DIR + '/index.js',
     vendor: VENDOR_LIBS
-  },*/
+  },
   output: {
     path: BUILD_DIR,
     filename: '[name].[hash].js',
@@ -55,6 +54,6 @@ const config = {
     }),
     new webpack.HotModuleReplacementPlugin()
   ]
-}
+};
 
 module.exports = config;
